2cd19fe7188871bce26dd8592a2e2f74ed9f9a5e
milsaoremo <milsaoremo@hotmail.com>
2025-02-27 03:27:02 +0000

    Codigo Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,35 @@
 //Teste larissa
 
 int main() {
-    printf("Desafio Super Trunfo\n");
+     char Estado[20] = "Bahia", Estado1[20] = "São Paulo", Codigo[20] = "A01", Codigo1[20] = "A02", cidade[30] = "Parateca",cidade1[30] = "São Bernardo";
+     int Populacao = 1809, Populacao1 = 20000;
+     float Area = 41780, Area1 = 4000000, PIB = 4000000, PIB1 = 2000000;
+     int pontosturisticos = 5, pontosturisticos1 = 50;
+
+
+     printf("Carta 1 \n");
+     printf("Estado: %s\n", Estado);
+     printf("Código: %s\n", Codigo);
+     printf("Nome da Cidade: %s\n", cidade);
+     printf("População: %d\n", Populacao);
+     printf("Área: %.0f km²\n", Area);
+     printf("PIB: %.0f\n", PIB);
+     printf("Número de Pontos Turísticos: %d\n", pontosturisticos);
+   
+//Super trunfo carta 2
+
+     printf("\n"); // Para pular uma Linha
+
+     printf("Carta 2 \n");
+     printf("Estado: %s\n", Estado1);
+     printf("Codigo: %s\n", Codigo1);
+     printf("Nome da Cidade: %s\n", cidade1);
+     printf("População :%d\n", Populacao1);
+     printf("Área: %0.f km²\n", Area1);
+     printf("PIB: %.0f\n", PIB1);
+     printf("Números de Pontos Turisticos %d\n", pontosturisticos1);
+
+
 
     
     
